c9ae50a7fec7f023a346f7a4c4e69a757a6772cc
hexaballs <zijin.lim@gmail.com>
2014-10-22 12:43:46 +0800

    changed the expectation content

--- spec/features/upload_image_spec.rb
+++ spec/features/upload_image_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'Visit the image uploader page' do
   scenario 'Upload Image' do
-    visit ('/')
-    expect(page).to have_content 'Image'
+    visit '/'
+    expect(page).to have_content 'Image Uploader'
   end
 end
